c0daccc25800ff32c4c35610a1ef6b100fc2552f
Carlos Didier Fernandez <didierfdz@gmail.com>
2019-06-10 19:52:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,57 @@
+
 // add solution here
+
+function theBeatlesPlay(musicians, instruments) {
+  var stringsArray = [];
+  
+  for (var i = 0; i < musicians.length; i += 1) {
+    var musician = musicians[i];
+    var instrument = instruments[i];
+    var newString = musician + ' plays ' + instrument;
+    stringsArray.push(newString);
+  }
+  
+  return stringsArray;
+}
+
+function johnLennonFacts(array) {
+  var factsArray = [];
+  
+  for (var i = 0; i < array.length; i += 1) {
+    var element = array[i];
+    var fact = element + '!!!';
+    factsArray.push(fact);
+  }
+  
+  return factsArray;
+}
+
+
+var i = 0;
+function incrementVariable() {
+  i = i + 1;
+  return i;
+}
+
+function iLoveTheBeatles(num) {
+  var newArray = [];
+  
+  do {
+    newArray.push('I love the Beatles!');
+  } while 
+  (incrementVariable() < 15);
+}
+
+// var i = 0;
+// function incrementVariable() {
+//   i = i + 1;
+//   return i;
+// }
+// function doWhileLoop(num) {
+//   do {
+//     console.log('I run once regardless');
+//   } while
+//   (incrementVariable() <= num);
+// }
+
+
